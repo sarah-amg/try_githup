8d42b1384a970ad7114fc405c03b62ada9aca79c
Amjad <amjad.88sd@gmail.com>
2022-11-14 13:41:53 +0300

    hi

---
+++ dart_application_folder_test/bin/dart_application_folder_test.dart
@@ -0,0 +1,9 @@
+import 'package:dart_application_folder_test/dart_application_folder_test.dart'
+    as dart_application_folder_test;
+
+import 'loop.dart';
+
+void main() {
+  
+  print(p());
+}
---
+++ dart_application_folder_test/test/dart_application_folder_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_folder_test/dart_application_folder_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
